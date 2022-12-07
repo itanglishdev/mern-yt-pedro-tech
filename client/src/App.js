dbd19433a08a9cd9314ba48c1b3a9230a67b0444
itanglishdev <chrishethertonweb@gmail.com>
2022-12-07 22:59:10 +0100

    added first css styling to input

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      HERE
+      <div className="inputs">
+        <input type="text" />
+        <input type="number" />
+      </div>
+
     </div>
   );
 }
